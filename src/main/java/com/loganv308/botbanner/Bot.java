f13c5dadbb7a712c87b1b68046437a1447373453
Logan Velier <Loganv308@gmail.com>
2025-02-26 20:19:51 -0600

    Moved project onto linux server, added corrected file path to grab latest logs.

--- src/main/java/com/loganv308/botbanner/Bot.java
+++ src/main/java/com/loganv308/botbanner/Bot.java
@@ -3,14 +3,14 @@ package com.loganv308.botbanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Bot {
     public static void main(String[] args) {
         try {
             // Change to log path in the MCServer Directory (/home/anton/mcserver/logs/latest.log)
-            File logs = new File("src/data/latest.log");
+            File logs = new File("/home/anton/mcserver/logs/latest.log");
             
             String ipv4Pattern = "\\b((25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\b";
 
